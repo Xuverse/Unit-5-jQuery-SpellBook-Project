83945b427cc6135cd092be8d61b1b9ae96aca113
Xuverse <45007308+Xuverse@users.noreply.github.com>
2019-03-05 09:23:08 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$(".disappear").click(function(){
+ $("#one").slideToggle();
+});
+
+
+
+//Spell #2
+
+$(".card-tricks").click(function(){
+ $("#two").fadeToggle();
+});
+
+
+
+//Spell #3
+
+$(".Dime-Penny").click(function(){
+ $("#three").slideToggle();
+});
+
+
+
